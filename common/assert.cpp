179059f04dd6b27e25cb3e41ec6365733702e6a8
Dimske <dempsey.de.clerck@hotmail.com>
2024-01-07 21:44:48 +0100

    Update assert.cpp

--- common/assert.cpp
+++ common/assert.cpp
@@ -6,12 +6,20 @@ namespace
 {
 
 #if !defined(_MSC_VER)
+    /// <summary>
+    /// Constructor function that initializes the CHECK_DEBUG_ENABLED flag.
+    /// </summary>
     __attribute__((constructor))
 #endif
         void initOnLoad()
     {
-        auto constexpr kDebugEnabled = "TRT_LLM_DEBUG_MODE";
+        auto constexpr kDebugEnabled = "DEBUG_MODE";
         auto const debugEnabled = std::getenv(kDebugEnabled);
+
+        /// <summary>
+        /// Check if the DEBUG_MODE environment variable is set to '1'
+        /// and set CHECK_DEBUG_ENABLED to true if it is.
+        /// </summary>
         if (debugEnabled && debugEnabled[0] == '1')
         {
             CHECK_DEBUG_ENABLED = true;
