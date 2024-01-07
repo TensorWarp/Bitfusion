fea6051b1114622bc01e996f505de9c2c016ebef
Dimske <dempsey.de.clerck@hotmail.com>
2024-01-07 15:49:40 +0100

    Update cudaAllocator.cpp

--- common/cudaAllocator.cpp
+++ common/cudaAllocator.cpp
@@ -1,57 +1,76 @@
 #include "cudaAllocator.h"
 #include "cudaUtils.h"
 
+#include <unordered_map>
 #include <utility>
 
-using namespace bitfusion::common;
-namespace tr = bitfusion::runtime;
+namespace bitfusion::common {
+    namespace tr = bitfusion::runtime;
 
-CudaAllocator::CudaAllocator(tr::BufferManager bufferManager)
-    : mBufferManager(std::move(bufferManager))
-{
-}
-
-ReallocType CudaAllocator::reallocType(void const* ptr, size_t size) const
-{
-    CHECK(contains(ptr));
-    auto const currentSize = mPointerMapping.at(ptr)->getSize();
-    LOG_DEBUG("current_buffer_size: %d, original buffer: %p, new buffer: %d", currentSize, ptr, size);
-    if (currentSize < size)
+    /// <summary>
+    /// Constructor for the CudaAllocator class.
+    /// </summary>
+    /// <param name="bufferManager">A BufferManager instance to manage GPU buffers.</param>
+    CudaAllocator::CudaAllocator(tr::BufferManager bufferManager)
+        : mBufferManager(std::move(bufferManager))
     {
-        return ReallocType::INCREASE;
     }
-    else if (currentSize == size)
-    {
-        return ReallocType::REUSE;
-    }
-    else
-    {
-        return ReallocType::DECREASE;
-    }
-}
 
-void* CudaAllocator::malloc(std::size_t size, bool const setZero)
-{
-    LOG_TRACE(__PRETTY_FUNCTION__);
-    auto bufferPtr = mBufferManager.gpu(size);
-    if (setZero)
+    /// <summary>
+    /// Determines the type of reallocation needed based on the current and requested sizes.
+    /// </summary>
+    /// <param name="ptr">Pointer to the current memory location.</param>
+    /// <param name="size">Requested size of memory.</param>
+    /// <returns>The type of reallocation needed (INCREASE, REUSE, or DECREASE).</returns>
+    ReallocType CudaAllocator::reallocType(const void* ptr, size_t size) const
     {
-        mBufferManager.setZero(*bufferPtr);
+        CHECK(contains(ptr));
+        const auto currentSize = mPointerMapping.at(ptr)->getSize();
+        LOG_DEBUG("current_buffer_size: %zu, original buffer: %p, new buffer: %zu", currentSize, ptr, size);
+        return (currentSize < size) ? ReallocType::INCREASE :
+            (currentSize == size) ? ReallocType::REUSE :
+            ReallocType::DECREASE;
     }
-    void* ptr{ bufferPtr->data() };
-    LOG_DEBUG("malloc buffer %p with size %ld", ptr, size);
-    mPointerMapping.insert({ ptr, std::move(bufferPtr) });
-    return ptr;
-}
 
-void CudaAllocator::free(void** ptr)
-{
-    LOG_TRACE(__PRETTY_FUNCTION__);
-    mPointerMapping.erase(*ptr);
-    *ptr = nullptr;
-}
+    /// <summary>
+    /// Allocates GPU memory and optionally sets it to zero.
+    /// </summary>
+    /// <param name="size">Size of memory to allocate.</param>
+    /// <param name="setZero">Flag indicating whether to initialize the memory to zero.</param>
+    /// <returns>A pointer to the allocated GPU memory.</returns>
+    void* CudaAllocator::malloc(size_t size, bool setZero)
+    {
+        LOG_TRACE(__PRETTY_FUNCTION__);
+        auto bufferPtr = mBufferManager.gpu(size);
+        if (setZero)
+        {
+            mBufferManager.setZero(*bufferPtr);
+        }
+        void* ptr = bufferPtr->data();
+        LOG_DEBUG("malloc buffer %p with size %zu", ptr, size);
+        mPointerMapping.emplace(ptr, std::move(bufferPtr));
+        return ptr;
+    }
 
-void CudaAllocator::memSet(void* ptr, int const val, size_t const size)
-{
-    check_cuda_error(cudaMemsetAsync(ptr, val, size, mBufferManager.getStream().get()));
+    /// <summary>
+    /// Frees GPU memory associated with the provided pointer.
+    /// </summary>
+    /// <param name="ptr">Pointer to the GPU memory to free.</param>
+    void CudaAllocator::free(void** ptr)
+    {
+        LOG_TRACE(__PRETTY_FUNCTION__);
+        mPointerMapping.erase(*ptr);
+        *ptr = nullptr;
+    }
+
+    /// <summary>
+    /// Sets GPU memory to a specified value asynchronously.
+    /// </summary>
+    /// <param name="ptr">Pointer to the GPU memory to set.</param>
+    /// <param name="val">Value to set in the memory.</param>
+    /// <param name="size">Size of the memory to set.</param>
+    void CudaAllocator::memSet(void* ptr, int val, size_t size)
+    {
+        check_cuda_error(cudaMemsetAsync(ptr, val, size, mBufferManager.getStream().get()));
+    }
 }
