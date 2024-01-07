5f11a649a357bff5151167ebbac483d9a27fae37
Dimske <dempsey.de.clerck@hotmail.com>
2024-01-07 15:44:40 +0100

    Update envUtils.cpp

--- common/envUtils.cpp
+++ common/envUtils.cpp
@@ -1,47 +1,75 @@
 #include "envUtils.h"
 #include "logger.h"
 #include <cstdlib>
+#include <optional>
+#include <string_view>
 
 namespace bitfusion::common
 {
+    namespace
+    {
+        std::optional<bool> forceMmhaMaxSeqLenTile;
+        std::optional<int> mmhaBlocksPerSequence;
+        
+        /// <summary>
+        /// Retrieves a boolean value from the environment variable.
+        /// </summary>
+        /// <param name="envVarName">The name of the environment variable.</param>
+        /// <returns>An optional boolean value. If the variable is not set or not equal to "1", returns nullopt.</returns>
+        std::optional<bool> getEnvBool(const char* envVarName)
+        {
+            if (const char* envValue = std::getenv(envVarName); envValue)
+            {
+                return std::string_view(envValue) == "1";
+            }
+            return std::nullopt;
+        }
 
+        /// <summary>
+        /// Retrieves an integer value from the environment variable.
+        /// </summary>
+        /// <param name="envVarName">The name of the environment variable.</param>
+        /// <returns>An optional integer value. If the variable is not set or not a valid integer, returns nullopt.</returns>
+        std::optional<int> getEnvInt(const char* envVarName)
+        {
+            if (const char* envValue = std::getenv(envVarName); envValue)
+            {
+                try
+                {
+                    return std::stoi(envValue);
+                }
+                catch (const std::invalid_argument&)
+                {
+                    LOG_WARNING("Invalid value for ", envVarName, ". Will use default values instead!");
+                }
+            }
+            return std::nullopt;
+        }
+    }
+
+    /// <summary>
+    /// Retrieves the environment variable TRENABLE_MMHA_MULTI_BLOCK_DEBUG and returns its boolean value.
+    /// </summary>
+    /// <returns>The boolean value of the environment variable or false if not set.</returns>
     bool getEnvMmhaMultiblockDebug()
     {
-        static bool init = false;
-        static bool forceMmhaMaxSeqLenTile = false;
-        if (!init)
+        if (!forceMmhaMaxSeqLenTile.has_value())
         {
-            init = true;
-            const char* enable_mmha_debug_var = std::getenv("TRENABLE_MMHA_MULTI_BLOCK_DEBUG");
-            if (enable_mmha_debug_var)
-            {
-                if (enable_mmha_debug_var[0] == '1' && enable_mmha_debug_var[1] == '\0')
-                {
-                    forceMmhaMaxSeqLenTile = true;
-                }
-            }
+            forceMmhaMaxSeqLenTile = getEnvBool("TRENABLE_MMHA_MULTI_BLOCK_DEBUG");
         }
-        return forceMmhaMaxSeqLenTile;
+        return forceMmhaMaxSeqLenTile.value_or(false);
     }
 
+    /// <summary>
+    /// Retrieves the environment variable TRMMHA_BLOCKS_PER_SEQUENCE and returns its integer value.
+    /// </summary>
+    /// <returns>The integer value of the environment variable or 0 if not set or not a valid integer.</returns>
     int getEnvMmhaBlocksPerSequence()
     {
-        static bool init = false;
-        static int mmhaBlocksPerSequence = 0;
-        if (!init)
+        if (!mmhaBlocksPerSequence.has_value())
         {
-            init = true;
-            const char* mmhaBlocksPerSequenceEnv = std::getenv("TRMMHA_BLOCKS_PER_SEQUENCE");
-            if (mmhaBlocksPerSequenceEnv)
-            {
-                mmhaBlocksPerSequence = std::atoi(mmhaBlocksPerSequenceEnv);
-                if (mmhaBlocksPerSequence <= 0)
-                {
-                    LOG_WARNING("Invalid value for TRMMHA_BLOCKS_PER_SEQUENCE. Will use default values instead!");
-                }
-            }
+            mmhaBlocksPerSequence = getEnvInt("TRMMHA_BLOCKS_PER_SEQUENCE");
         }
-        return mmhaBlocksPerSequence;
+        return mmhaBlocksPerSequence.value_or(0);
     }
-
 }
