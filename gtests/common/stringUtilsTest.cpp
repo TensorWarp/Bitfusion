0076a81a53bb894d9af5f3f927c67ed04afe53a7
Dimske <dempsey.de.clerck@hotmail.com>
2024-01-07 16:05:23 +0100

    Update stringUtilsTest.cpp

--- gtests/common/stringUtilsTest.cpp
+++ gtests/common/stringUtilsTest.cpp
@@ -1,64 +1,85 @@
-
 #include <gtest/gtest.h>
-
 #include "../../common/stringUtils.h"
-
 #include <sstream>
+#include <string>
+#include <iomanip>
 
 using namespace bitfusion::common;
 
-namespace
-{
+namespace {
 
-auto constexpr SHORT_STRING = "Let's format this string 5 times.";
+    ///<summary>
+    /// A short string used for formatting.
+    ///</summary>
+    constexpr auto SHORT_STRING = "Let's format this string 5 times.";
 
-inline std::string formatShort()
-{
-    return fmtstr("Let's %s this string %d times.", "format", 5);
-}
+    ///<summary>
+    /// Format a short string.
+    ///</summary>
+    ///<returns>The formatted short string.</returns>
+    inline std::string formatShort() {
+        return fmtstr("Let's %s this string %d times.", "format", 5);
+    }
 
-auto const LONG_STRING = std::string(10000, '?');
-auto constexpr LONG_PREFIX = "add me";
+    ///<summary>
+    /// A long string used for formatting.
+    ///</summary>
+    auto const LONG_STRING = std::string(10000, '?');
+    ///<summary>
+    /// Prefix for the long string.
+    ///</summary>
+    auto constexpr LONG_PREFIX = "add me";
 
-std::string formatLong()
-{
-    return fmtstr("add me%s", LONG_STRING.c_str());
-}
+    ///<summary>
+    /// Format a long string.
+    ///</summary>
+    ///<returns>The formatted long string.</returns>
+    std::string formatLong() {
+        return LONG_PREFIX + LONG_STRING;
+    }
 
-std::ostringstream priceFormatStream;
+    std::ostringstream priceFormatStream;
 
-template <typename P>
-std::string formatFixed(P price)
-{
-    priceFormatStream.str("");
-    priceFormatStream.clear();
-    priceFormatStream << price;
-    return priceFormatStream.str();
-}
+    ///<summary>
+    /// Format a price with a given precision.
+    ///</summary>
+    ///<param name="price">The price to format.</param>
+    ///<param name="precision">The precision for formatting.</param>
+    ///<returns>The formatted price as a string.</returns>
+    template <typename P>
+    std::string formatFixed(P price, int precision) {
+        priceFormatStream.str("");
+        priceFormatStream << std::fixed << std::setprecision(precision) << price;
+        return priceFormatStream.str();
+    }
 
-}
+} // namespace
 
-TEST(StringUtil, ShortStringFormat)
-{
+///<summary>
+/// Test case for formatting a short string.
+///</summary>
+TEST(StringUtil, ShortStringFormat) {
     EXPECT_EQ(SHORT_STRING, formatShort());
 }
 
-TEST(StringUtil, LongStringFormat)
-{
+///<summary>
+/// Test case for formatting a long string.
+///</summary>
+TEST(StringUtil, LongStringFormat) {
     EXPECT_EQ(LONG_PREFIX + LONG_STRING, formatLong());
 }
 
-TEST(StringUtil, FormatFixedDecimals)
-{
-    auto num = 0.123456789;
+///<summary>
+/// Test case for formatting a price with different precisions.
+///</summary>
+TEST(StringUtil, FormatFixedDecimals) {
+    constexpr auto num = 0.123456789;
 
-    for (auto d = 1; d <= 9; ++d)
-    {
-        auto const fmt = std::string("%.") + std::to_string(d) + "f";
+    for (auto d = 1; d <= 9; ++d) {
+        auto const fmt = fmtstr("%%.%df", d);
         auto prefix = fmtstr(fmt.c_str(), num);
-        priceFormatStream.precision(d);
-        EXPECT_EQ(prefix, formatFixed(num));
-        EXPECT_EQ(prefix, formatFixed(num));
-        EXPECT_EQ(prefix, formatFixed(num));
+        EXPECT_EQ(prefix, formatFixed(num, d));
+        EXPECT_EQ(prefix, formatFixed(num, d));
+        EXPECT_EQ(prefix, formatFixed(num, d));
     }
 }
