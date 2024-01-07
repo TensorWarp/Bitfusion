a37893f51d5ddc9ca40241cbed65ff3da541fa51
Dimske <dempsey.de.clerck@hotmail.com>
2024-01-07 15:56:32 +0100

    Update exceptionTest.cpp

--- gtests/common/exceptionTest.cpp
+++ gtests/common/exceptionTest.cpp
@@ -1,45 +1,54 @@
-
-#include <gmock/gmock-matchers.h>
 #include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
 
-#include "../../common/assert.h"
+#include <stdexcept>
+#include <iostream>
 #include "../../common/logger.h"
-#include "../../common/exception.h"
-
-#include <string>
 
 using ::testing::HasSubstr;
 
-TEST(TllmException, StackTrace)
+/// <summary>
+/// Test case for StackTrace function.
+/// </summary>
+TEST(Exception, StackTrace)
 {
-    auto ex = NEW_EXCEPTION("TestException %d", 1);
-    std::string const what{ex.what()};
-    EXPECT_THAT(what, HasSubstr(std::to_string(__LINE__ - 2)));
-    EXPECT_THAT(what, HasSubstr("TestException 1"));
-    EXPECT_THAT(what, HasSubstr(__FILE__));
+    try
+    {
+        throw std::runtime_error("TestException 1");
+    }
+    catch (const std::exception& e)
+    {
+        const std::string what = e.what();
+        EXPECT_THAT(what, HasSubstr(std::to_string(__LINE__ - 6)));
+        EXPECT_THAT(what, HasSubstr("TestException 1"));
+        EXPECT_THAT(what, HasSubstr(__FILE__));
 #if !defined(_MSC_VER)
-    EXPECT_THAT(what, ::testing::Not(HasSubstr("bitfusion::common::TllmException::TllmException")));
-    EXPECT_THAT(what, HasSubstr("tests/tllmExceptionTest"));
-    EXPECT_THAT(what, HasSubstr("main"));
+        EXPECT_THAT(what, ::testing::Not(HasSubstr("bitfusion::common::Exception::Exception")));
+        EXPECT_THAT(what, HasSubstr("tests/tllmExceptionTest"));
+        EXPECT_THAT(what, HasSubstr("main"));
 #endif
+    }
 }
 
+/// <summary>
+/// Test case for Logger function.
+/// </summary>
 TEST(Exception, Logger)
 {
     try
     {
-        THROW("TestException %d", 1);
+        throw std::runtime_error("TestException 1");
     }
     catch (const std::exception& e)
     {
         testing::internal::CaptureStdout();
         LOG_EXCEPTION(e);
         auto const out = testing::internal::GetCapturedStdout();
-        EXPECT_THAT(out, HasSubstr(std::to_string(__LINE__ - 7)));
+        EXPECT_THAT(out, HasSubstr(std::to_string(__LINE__ - 8)));
         EXPECT_THAT(out, HasSubstr("TestException 1"));
         EXPECT_THAT(out, HasSubstr(__FILE__));
 #if !defined(_MSC_VER)
-        EXPECT_THAT(out, ::testing::Not(HasSubstr("bitfusion::common::TllmException::TllmException")));
+        EXPECT_THAT(out, ::testing::Not(HasSubstr("bitfusion::common::Exception::Exception")));
         EXPECT_THAT(out, HasSubstr("tests/tllmExceptionTest"));
         EXPECT_THAT(out, HasSubstr("main"));
 #endif
