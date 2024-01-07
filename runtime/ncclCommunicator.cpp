239de6be3f8acd13d5adf81041dd2b5397bc7a0c
Dimske <dempsey.de.clerck@hotmail.com>
2024-01-07 22:49:26 +0100

    Update ncclCommunicator.cpp

--- runtime/ncclCommunicator.cpp
+++ runtime/ncclCommunicator.cpp
@@ -1,8 +1,5 @@
-
 #include "ncclCommunicator.h"
-
 #include "../runtime/utils/multiDeviceUtils.h"
-
 #include <NvInferRuntime.h>
 #include <mpi.h>
 #include <type_traits>
@@ -16,45 +13,46 @@ using namespace bitfusion::runtime;
 namespace
 {
 #if ENABLE_MULTI_DEVICE
-template <typename T>
-struct NcclDataType
-{
-};
-
-template <>
-struct NcclDataType<half>
-{
-    static constexpr auto value = ncclDataType_t::ncclHalf;
-};
-
-template <>
-struct NcclDataType<float>
-{
-    static constexpr auto value = ncclDataType_t::ncclFloat;
-};
-
-template <>
-struct NcclDataType<std::uint8_t>
-{
-    static constexpr auto value = ncclDataType_t::ncclUint8;
-};
-
-template <>
-struct NcclDataType<std::int32_t>
-{
-    static constexpr auto value = ncclDataType_t::ncclInt32;
-};
+    /// <summary>
+    /// Template class for mapping C++ types to NCCL data types.
+    /// </summary>
+    template <typename T>
+    constexpr auto NcclDataType_v = [] {
+        if constexpr (std::is_same_v<T, half>) {
+            return ncclDataType_t::ncclHalf;
+        }
+        else if constexpr (std::is_same_v<T, float>) {
+            return ncclDataType_t::ncclFloat;
+        }
+        else if constexpr (std::is_same_v<T, std::uint8_t>) {
+            return ncclDataType_t::ncclUint8;
+        }
+        else if constexpr (std::is_same_v<T, std::int32_t>) {
+            return ncclDataType_t::ncclInt32;
+        }
+        else {
+            return ncclDataType_t::ncclInt32; // Default to ncclInt32
+        }
+        };
 #endif
 }
 
+/// <summary>
+/// Send data using NCCL.
+/// </summary>
+/// <typeparam name="T">The data type.</typeparam>
+/// <param name="sendbuff">Pointer to the data to send.</param>
+/// <param name="count">The number of elements to send.</param>
+/// <param name="peer">The peer rank to send data to.</param>
+/// <param name="stream">The CUDA stream.</param>
 template <typename T>
 void NcclCommunicator::send(T* sendbuff, size_t count, int peer, CudaStream const& stream) const
 {
 #if ENABLE_MULTI_DEVICE
-    auto datatype = NcclDataType<std::remove_cv_t<T>>::value;
+    auto datatype = NcclDataType_v<T>();
     NCCL_CHECK(ncclSend(sendbuff, count, datatype, peer, mComm, stream.get()));
 #else
-    THROW("Multi device support is disabled.");
+    THROW("Multi-device support is disabled.");
 #endif
 }
 
@@ -64,14 +62,22 @@ template void NcclCommunicator::send(std::uint8_t const*, size_t, int, CudaStrea
 template void NcclCommunicator::send(std::int32_t const*, size_t, int, CudaStream const&) const;
 template void NcclCommunicator::send(float const*, size_t, int, CudaStream const&) const;
 
+/// <summary>
+/// Receive data using NCCL.
+/// </summary>
+/// <typeparam name="T">The data type.</typeparam>
+/// <param name="sendbuff">Pointer to the receive buffer.</param>
+/// <param name="count">The number of elements to receive.</param>
+/// <param name="peer">The peer rank to receive data from.</param>
+/// <param name="stream">The CUDA stream.</param>
 template <typename T>
 void NcclCommunicator::receive(T* sendbuff, size_t count, int peer, CudaStream const& stream) const
 {
 #if ENABLE_MULTI_DEVICE
-    auto datatype = NcclDataType<std::remove_cv_t<T>>::value;
+    auto datatype = NcclDataType_v<T>();
     NCCL_CHECK(ncclRecv(sendbuff, count, datatype, peer, mComm, stream.get()));
 #else
-    THROW("Multi device support is disabled.");
+    THROW("Multi-device support is disabled.");
 #endif
 }
 
@@ -79,6 +85,11 @@ template void NcclCommunicator::receive(std::uint8_t*, size_t, int, CudaStream c
 template void NcclCommunicator::receive(std::int32_t*, size_t, int, CudaStream const&) const;
 template void NcclCommunicator::receive(float*, size_t, int, CudaStream const&) const;
 
+/// <summary>
+/// Create an NCCL communicator for pipeline communication.
+/// </summary>
+/// <param name="worldConfig">The world configuration.</param>
+/// <returns>The created NCCL communicator.</returns>
 std::shared_ptr<NcclCommunicator> NcclCommunicator::createPipelineComm(WorldConfig const& worldConfig)
 {
 #if ENABLE_MULTI_DEVICE
