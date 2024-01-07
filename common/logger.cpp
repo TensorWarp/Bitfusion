b9054517e0a76aae922584e31d957a17155b3ad0
Dimske <dempsey.de.clerck@hotmail.com>
2024-01-07 21:08:32 +0100

    Update logger.cpp

--- common/logger.cpp
+++ common/logger.cpp
@@ -1,12 +1,14 @@
 #include "logger.h"
 #include "exception.h"
 #include <cuda_runtime.h>
+#include <map>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
 
-namespace bitfusion::common
-{
+namespace bitfusion::common {
 
-    Logger::Logger()
-    {
+    Logger::Logger() {
         char* isFirstRankOnlyChar = std::getenv("LOG_FIRST_RANK_ONLY");
         bool isFirstRankOnly = (isFirstRankOnlyChar != nullptr && std::string(isFirstRankOnlyChar) == "ON");
 
@@ -14,28 +16,25 @@ namespace bitfusion::common
         cudaGetDevice(&deviceId);
 
         char* levelName = std::getenv("LOG_LEVEL");
-        if (levelName != nullptr)
-        {
-            std::map<std::string, Level> nameToLevel = {
+        if (levelName != nullptr) {
+            static const std::map<std::string, Level> nameToLevel = {
                 {"TRACE", TRACE},
                 {"DEBUG", DEBUG},
                 {"INFO", INFO},
                 {"WARNING", WARNING},
                 {"ERROR", ERROR},
             };
+
             auto level = nameToLevel.find(levelName);
-            if (isFirstRankOnly && deviceId != 0)
-            {
+            if (isFirstRankOnly && deviceId != 0) {
                 level = nameToLevel.find("ERROR");
             }
-            if (level != nameToLevel.end())
-            {
+            if (level != nameToLevel.end()) {
                 setLevel(level->second);
             }
-            else
-            {
-                fprintf(stderr,
-                    "[TensorRT-LLM][WARNING] Invalid logger level LOG_LEVEL=%s. "
+            else {
+                std::fprintf(stderr,
+                    "[WARNING] Invalid logger level LOG_LEVEL=%s. "
                     "Ignore the environment variable and use a default "
                     "logging level.\n",
                     levelName);
@@ -44,15 +43,12 @@ namespace bitfusion::common
         }
     }
 
-    void Logger::log(std::exception const& ex, Logger::Level level)
-    {
+    void Logger::log(const std::exception& ex, Level level) {
         log(level, "%s: %s", Exception::demangle(typeid(ex).name()).c_str(), ex.what());
     }
 
-    Logger* Logger::getLogger()
-    {
+    Logger* Logger::getLogger() {
         thread_local Logger instance;
         return &instance;
     }
-
 }
