98916220706ece1737bf02190db735def160d3d3
Dimske <dempsey.de.clerck@hotmail.com>
2024-01-08 01:52:33 +0100

    Create logger.h

---
+++ runtime/logger.h
@@ -0,0 +1,19 @@
+
+#pragma once
+
+#include <NvInferRuntime.h>
+
+namespace bitfusion::runtime
+{
+
+class Logger : public nvinfer1::ILogger
+{
+public:
+    void log(Severity severity, nvinfer1::AsciiChar const* msg) noexcept override;
+
+    Severity getLevel();
+
+    void setLevel(Severity level);
+};
+
+}
