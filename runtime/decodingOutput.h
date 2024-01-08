0a460c0bbd743b6980e5834145cc9cc83004a3d4
Dimske <dempsey.de.clerck@hotmail.com>
2024-01-08 02:21:10 +0100

    Update decodingOutput.h

--- runtime/decodingOutput.h
+++ runtime/decodingOutput.h
@@ -1,4 +1,3 @@
-
 #pragma once
 
 #include "../common/cudaUtils.h"
@@ -10,57 +9,87 @@
 
 namespace bitfusion::runtime
 {
-class DecodingOutput
-{
-public:
-    using TensorPtr = ITensor::SharedPtr;
-
-    class BeamHypotheses
+    /// <summary>
+    /// Class representing the output of a decoding operation.
+    /// </summary>
+    class DecodingOutput
     {
     public:
-        TensorPtr outputIdsTgt;
-        TensorPtr sequenceLengthsTgt;
-        TensorPtr cumLogProbs;
-        TensorPtr normedScores;
+        using TensorPtr = ITensor::SharedPtr;
+
+        /// <summary>
+        /// Class representing beam hypotheses during decoding.
+        /// </summary>
+        class BeamHypotheses
+        {
+        public:
+            TensorPtr outputIdsTgt;
+            TensorPtr sequenceLengthsTgt;
+            TensorPtr cumLogProbs;
+            TensorPtr normedScores;
+            TensorPtr logProbs;
+            TensorPtr minNormedScores;
+            TensorPtr numBeams;
+            TensorPtr isDone;
+
+            /// <summary>
+            /// Clear and release resources associated with the beam hypotheses.
+            /// </summary>
+            /// <param name="manager">Buffer manager for resource management.</param>
+            void empty(BufferManager& manager);
+
+            /// <summary>
+            /// Reshape the beam hypotheses tensors.
+            /// </summary>
+            /// <param name="batchSize">Size of the batch.</param>
+            /// <param name="beamWidth">Beam width.</param>
+            /// <param name="maxSequenceLength">Maximum sequence length.</param>
+            void reshape(SizeType batchSize, SizeType beamWidth, SizeType maxSequenceLength);
+
+            /// <summary>
+            /// Release resources associated with the beam hypotheses.
+            /// </summary>
+            void release();
+
+            /// <summary>
+            /// Initialize the beam hypotheses.
+            /// </summary>
+            /// <param name="manager">Buffer manager for resource management.</param>
+            /// <param name="endId">End identifier for decoding.</param>
+            void init(BufferManager& manager, TokenIdType endId);
+
+            /// <summary>
+            /// Create a slice of the beam hypotheses.
+            /// </summary>
+            /// <param name="batchIndex">Batch index for the slice.</param>
+            /// <param name="size">Size of the slice.</param>
+            /// <returns>A new BeamHypotheses object representing the slice.</returns>
+            BeamHypotheses slice(SizeType batchIndex, SizeType size) const;
+        };
+
+        static float constexpr kNegativeInfinity = -1e20f;
+
+        /// <summary>
+        /// Constructor for DecodingOutput.
+        /// </summary>
+        /// <param name="ids">Pointer to the output tensor for IDs.</param>
+        explicit DecodingOutput(TensorPtr ids)
+            : ids{ std::move(ids) }
+        {
+            CHECK_WITH_INFO(static_cast<bool>(this->ids), "Invalid ids tensor");
+        }
+
+        TensorPtr ids;
+        TensorPtr newTokensSteps;
+        TensorPtr newTokens;
+        std::vector<TensorPtr> newTokensVec;
+        TensorPtr finished;
+        TensorPtr finishedSum;
         TensorPtr logProbs;
-        TensorPtr minNormedScores;
-        TensorPtr numBeams;
-        TensorPtr isDone;
-
-        void empty(BufferManager& manager);
-
-        void reshape(SizeType batchSize, SizeType beamWidth, SizeType maxSequenceLength);
-
-        void release();
-
-        void init(BufferManager& manager, TokenIdType endId);
-
-        BeamHypotheses slice(SizeType batchIndex, SizeType size) const;
+        TensorPtr cumLogProbs;
+        TensorPtr parentIds;
+        TensorPtr lengths;
+        TensorPtr cacheIndirection;
+        BeamHypotheses beamHypotheses;
     };
-
-    static float constexpr kNegativeInfinity = -1e20f;
-
-    explicit DecodingOutput(TensorPtr ids)
-        : ids{std::move(ids)}
-    {
-        CHECK_WITH_INFO(static_cast<bool>(this->ids), "Invalid ids tensor");
-    }
-
-    TensorPtr ids;
-    TensorPtr newTokensSteps;
-    TensorPtr newTokens;
-    std::vector<TensorPtr> newTokensVec;
-
-    TensorPtr finished;
-    TensorPtr finishedSum;
-
-    TensorPtr logProbs;
-    TensorPtr cumLogProbs;
-    TensorPtr parentIds;
-    TensorPtr lengths;
-    TensorPtr cacheIndirection;
-
-    BeamHypotheses beamHypotheses;
-};
-
 }
