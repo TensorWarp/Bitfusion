73b9db1fe14eec8bf92f1d68846342b8b3fe603c
Dimske <dempsey.de.clerck@hotmail.com>
2024-01-08 02:20:16 +0100

    Update generationInput.h

--- runtime/generationInput.h
+++ runtime/generationInput.h
@@ -1,4 +1,3 @@
-
 #pragma once
 
 #include "../common/cudaUtils.h"
@@ -12,50 +11,73 @@
 namespace bitfusion::runtime
 {
 
-template <typename TTensor, typename PromptTuningParams>
-class GenericGenerationInput
-{
-public:
-    using TensorPtr = TTensor;
-
-    explicit GenericGenerationInput(
-        SizeType const endId, SizeType const padId, TensorPtr ids, TensorPtr lengths, bool packed = false)
-        : endId{endId}
-        , padId{padId}
-        , ids{std::move(ids)}
-        , lengths{std::move(lengths)}
-        , packed{packed}
-        , maxNewTokens(std::nullopt)
+    /// <summary>
+    /// Template class for handling generic generation input data.
+    /// </summary>
+    /// <typeparam name="TTensor">Type of the tensor used for generation input.</typeparam>
+    /// <typeparam name="PromptTuningParams">Type of prompt tuning parameters.</typeparam>
+    template <typename TTensor, typename PromptTuningParams>
+    class GenericGenerationInput
     {
-        CHECK_WITH_INFO(static_cast<bool>(this->ids), "Invalid ids tensor");
-        CHECK_WITH_INFO(static_cast<bool>(this->lengths), "Invalid lengths tensor");
-    }
+    public:
+        using TensorPtr = TTensor;
 
-    SizeType endId;
-    SizeType padId;
-    TensorPtr ids;
-    TensorPtr lengths;
-    bool packed;
+        /// <summary>
+        /// Constructor for GenericGenerationInput.
+        /// </summary>
+        /// <param name="endId">End identifier for generation input.</param>
+        /// <param name="padId">Padding identifier for generation input.</param>
+        /// <param name="ids">Pointer to the input tensor for IDs.</param>
+        /// <param name="lengths">Pointer to the input tensor for lengths.</param>
+        /// <param name="packed">Boolean indicating if input is packed.</param>
+        explicit GenericGenerationInput(
+            SizeType const endId, SizeType const padId, TensorPtr ids, TensorPtr lengths, bool packed = false)
+            : endId{ endId }
+            , padId{ padId }
+            , ids{ std::move(ids) }
+            , lengths{ std::move(lengths) }
+            , packed{ packed }
+            , maxNewTokens(std::nullopt)
+        {
+            CHECK_WITH_INFO(static_cast<bool>(this->ids), "Invalid ids tensor");
+            CHECK_WITH_INFO(static_cast<bool>(this->lengths), "Invalid lengths tensor");
+        }
 
-    TensorPtr embeddingBias;
-    TensorPtr badWordsList;
-    TensorPtr stopWordsList;
-    std::optional<SizeType> maxNewTokens;
+        SizeType endId;
+        SizeType padId;
+        TensorPtr ids;
+        TensorPtr lengths;
+        bool packed;
 
-    PromptTuningParams promptTuningParams;
-};
+        TensorPtr embeddingBias;
+        TensorPtr badWordsList;
+        TensorPtr stopWordsList;
+        std::optional<SizeType> maxNewTokens;
 
-class GenerationInput : public GenericGenerationInput<ITensor::SharedPtr, PromptTuningParams>
-{
-public:
-    using Base = GenericGenerationInput<ITensor::SharedPtr, PromptTuningParams>;
-    using TensorPtr = Base::TensorPtr;
+        PromptTuningParams promptTuningParams;
+    };
 
-    explicit GenerationInput(
-        SizeType const endId, SizeType const padId, TensorPtr ids, TensorPtr lengths, bool packed = false)
-        : GenericGenerationInput(endId, padId, std::move(ids), std::move(lengths), packed)
+    /// <summary>
+    /// Class for handling generation input data.
+    /// </summary>
+    class GenerationInput : public GenericGenerationInput<ITensor::SharedPtr, PromptTuningParams>
     {
-    }
-};
+    public:
+        using Base = GenericGenerationInput<ITensor::SharedPtr, PromptTuningParams>;
+        using TensorPtr = Base::TensorPtr;
 
+        /// <summary>
+        /// Constructor for GenerationInput.
+        /// </summary>
+        /// <param name="endId">End identifier for generation input.</param>
+        /// <param name="padId">Padding identifier for generation input.</param>
+        /// <param name="ids">Pointer to the input tensor for IDs.</param>
+        /// <param name="lengths">Pointer to the input tensor for lengths.</param>
+        /// <param name="packed">Boolean indicating if input is packed.</param>
+        explicit GenerationInput(
+            SizeType const endId, SizeType const padId, TensorPtr ids, TensorPtr lengths, bool packed = false)
+            : GenericGenerationInput(endId, padId, std::move(ids), std::move(lengths), packed)
+        {
+        }
+    };
 }
